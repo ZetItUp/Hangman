7f8d3bbd5f753fbfb311fa77795ce24735e9e0c0
Andreas Lindström <zetitup@gmail.com>
2025-06-25 10:02:04 +0200

    Guessing

--- Hangman/Hangman/Screens/GameScreen.cs
+++ Hangman/Hangman/Screens/GameScreen.cs
@@ -11,18 +11,31 @@ namespace Hangman.Screens
     public class GameScreen : Screen
     {
         string currentWord = string.Empty;
+        string guessedLetters = string.Empty;
 
         bool updateWord = true;
         public GameScreen()
         {
-            Title = "Hangman Game";
+            Title = "Hangman";
             Description = "Guess the word before you run out of attempts!";
             currentWord = GenerateNewWord();
         }
 
         public override void Update()
         {
-            if(FirstDraw)
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+
+            if (PrevWindowHeight != height || PrevWindowWidth != width)
+            {
+                DrawGame();
+                DrawWord();
+
+                PrevWindowWidth = width;
+                PrevWindowHeight = height;
+            }
+
+            if (FirstDraw)
             {
                 DrawGame();
             }
@@ -32,17 +45,49 @@ namespace Hangman.Screens
             {
                 updateWord = false;
                 DrawWord();
+                DrawGuessedLetters();
             }
 
             if(Console.KeyAvailable)
             {
-                var key = Console.ReadKey(true).Key;
-                switch (key)
+                var keyInfo = Console.ReadKey(true);
+                var key = keyInfo.Key;
+                
+                if(key >= ConsoleKey.A && key <= ConsoleKey.Z)
                 {
-                    case ConsoleKey.Escape:
-                        ScreenManager.SetScreen(ScreenManager.ScreenID.MainMenu);
-                        Reset();
-                        break;
+                    char guessedLetter = char.ToLower(keyInfo.KeyChar);
+
+                    if(!guessedLetters.Contains(guessedLetter))
+                    {
+                        guessedLetters += guessedLetter;
+                        updateWord = true;
+
+                        Console.SetCursorPosition(1, Console.WindowHeight - 9);
+                        ConsoleExt.WriteCentered($"                                  ", ConsoleColor.Gray, ConsoleColor.Black, Console.WindowWidth);
+
+                        if (currentWord.ToLower().Contains(guessedLetter))
+                        {
+                            Console.SetCursorPosition(0, Console.WindowHeight - 9);
+                            ConsoleExt.WriteCentered($"Good guess: {guessedLetter}", ConsoleColor.Green, ConsoleColor.Black, Console.WindowWidth);
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(0, Console.WindowHeight - 9);
+                            ConsoleExt.WriteCentered($"Wrong guess: {guessedLetter}", ConsoleColor.Red, ConsoleColor.Black, Console.WindowWidth);
+                        }
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(1, Console.WindowHeight - 9);
+                        ConsoleExt.WriteCentered($"                                  ", ConsoleColor.Gray, ConsoleColor.Black, Console.WindowWidth);
+                        Console.SetCursorPosition(0, Console.WindowHeight - 9);
+                        ConsoleExt.WriteCentered($"You already guessed: {guessedLetter}", ConsoleColor.Yellow, ConsoleColor.Black, Console.WindowWidth);
+                    }
+                }
+                else if (key == ConsoleKey.Escape)
+                {
+                    ScreenManager.SetScreen(ScreenManager.ScreenID.MainMenu);
+                    Reset();
                 }
             }
 
@@ -71,20 +116,36 @@ namespace Hangman.Screens
 
         private void DrawWord()
         {
-            DrawBox(2, 5, Console.WindowWidth - 4, 3, ConsoleColor.White, ConsoleColor.Black);
-            Console.SetCursorPosition(3, 6);
-            Console.ForegroundColor = ConsoleColor.Green;
+            DrawBox(2, 6, Console.WindowWidth - 4, 3, ConsoleColor.Yellow, ConsoleColor.Black);
+            Console.SetCursorPosition(4, 7);
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("Current Word: ");
-            for(int i = 0; i < currentWord.Length; i++)
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            for (int i = 0; i < currentWord.Length; i++)
             {
-                if (char.IsLetter(currentWord[i]))
+                if( guessedLetters.Contains(currentWord[i].ToString().ToLower()))
                 {
-                    Console.Write("_ ");
+                    Console.Write($"{currentWord[i].ToString().ToUpper()} ");
                 }
                 else
                 {
-                    Console.Write(currentWord[i] + " ");
+                    Console.Write("_ ");
                 }
+            }
+        }
+
+        private void DrawGuessedLetters()
+        {
+            DrawBox(2, Console.WindowHeight - 8, Console.WindowWidth - 4, 3, ConsoleColor.Red, ConsoleColor.Black);
+            Console.SetCursorPosition(4, Console.WindowHeight - 7);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Guessed Letters: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (int i = 0; i < guessedLetters.Length; i++)
+            {
+                Console.Write($"{guessedLetters[i].ToString().ToUpper()} ");
             }
         }
 
@@ -96,6 +157,9 @@ namespace Hangman.Screens
             Console.ForegroundColor = ConsoleColor.Gray;
             ConsoleExt.WriteCentered(Title, ConsoleColor.Yellow, ConsoleColor.Black, Console.WindowWidth);
             ConsoleExt.WriteCentered(Description, ConsoleColor.Gray, ConsoleColor.Black, Console.WindowWidth);
+
+            Console.SetCursorPosition(0, Console.WindowHeight - 2);
+            ConsoleExt.WriteCentered("Press ESC to return to the main menu.", ConsoleColor.Cyan, ConsoleColor.Black, Console.WindowWidth);
         }
     }
 }
